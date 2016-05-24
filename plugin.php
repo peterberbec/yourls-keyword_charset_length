512c694780c4e658507d896491dd09e947fdf917
Peter Berbec <peter@berbec.com>
2016-05-24 10:59:29 -0400

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -1,17 +1,27 @@
 <?php
 /*
-Plugin Name: Simple Charset
-Plugin URI: https://github.com/giveforward/yourls-simplecharset
-Description: allows a simple charset override
-Version: 1.1
-Author: gfwd
-Author URI: http://www.giveforward.com
+Plugin Name: Simple Random Charset
+trying to run off ozh's random-keywords instead
 */
 
 
-yourls_add_filter('get_shorturl_charset', 'gfwd_get_shorturl_charset' );
-
-function gfwd_get_shorturl_charset( $charset )
-{
-  return defined('YOURLS_GFWD_CHARSET') ? YOURLS_GFWD_CHARSET : $charset;
+global $prb_random_keyword;
+/*
+* CONFIG: EDIT THIS
+*/
+/* Length of random keyword */
+$prb_random_keyword['length'] = 5;
+/*
+* DO NOT EDIT FARTHER
+*/
+// Generate a random keyword
+yourls_add_filter( 'random_keyword', 'prb_random_keyword' );
+function prb_random_keyword() {
+        global $prb_random_keyword;
+        return yourls_rnd_string( $prb_random_keyword['length'], 0, defined('YOURLS_GFWD_CHARSET') ? YOURLS_GFWD_CHARSET : $charset );
+}
+// Don't increment sequential keyword tracker
+yourls_add_filter( 'get_next_decimal', 'prb_random_keyword_next_decimal' );
+function prb_random_keyword_next_decimal( $next ) {
+        return ( $next - 1 );
 }
